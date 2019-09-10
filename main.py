8ca6e5d778cdac508631991e9ac7bd26b7fe9eb2
fan tang <46098058+tfxxxxxx@users.noreply.github.com>
2019-09-10 18:07:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=256, metavar='N',
                     help='input batch size for testing (default: 256)')
-parser.add_argument('--epochs', type=int, default=160, metavar='N',
-                    help='number of epochs to train (default: 160)')
+parser.add_argument('--epochs', type=int, default=20, metavar='N',
+                    help='number of epochs to train (default: 20)')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
